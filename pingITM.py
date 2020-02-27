cdba6fe0a6598154aeeaf73250deeda3b66a7185
therams2 <61556251+therams2@users.noreply.github.com>
2020-02-27 12:53:18 -0600

    Update pingITM.py

--- pingITM.py
+++ pingITM.py
@@ -13,4 +13,479 @@ for i in range(254):
   print (hostname+repr(i) + "no esta funcionamiento!")
 
 
+
+
+Haciendo ping a 200.33.171.0 con 32 bytes de datos:
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+
+Estadísticas de ping para 200.33.171.0:
+    Paquetes: enviados = 4, recibidos = 4, perdidos = 0
+    (0% perdidos),
+200.33.171.0En funcionamiento: !0
+
+Haciendo ping a 200.33.171.1 con 32 bytes de datos:
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+
+Estadísticas de ping para 200.33.171.1:
+    Paquetes: enviados = 4, recibidos = 4, perdidos = 0
+    (0% perdidos),
+200.33.171.1En funcionamiento: !0
+
+Haciendo ping a 200.33.171.2 con 32 bytes de datos:
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+
+Estadísticas de ping para 200.33.171.2:
+    Paquetes: enviados = 4, recibidos = 4, perdidos = 0
+    (0% perdidos),
+200.33.171.2En funcionamiento: !0
+
+Haciendo ping a 200.33.171.3 con 32 bytes de datos:
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+
+Estadísticas de ping para 200.33.171.3:
+    Paquetes: enviados = 4, recibidos = 4, perdidos = 0
+    (0% perdidos),
+200.33.171.3En funcionamiento: !0
+
+Haciendo ping a 200.33.171.4 con 32 bytes de datos:
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+
+Estadísticas de ping para 200.33.171.4:
+    Paquetes: enviados = 4, recibidos = 4, perdidos = 0
+    (0% perdidos),
+200.33.171.4En funcionamiento: !0
+
+Haciendo ping a 200.33.171.5 con 32 bytes de datos:
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+
+Estadísticas de ping para 200.33.171.5:
+    Paquetes: enviados = 4, recibidos = 4, perdidos = 0
+    (0% perdidos),
+200.33.171.5En funcionamiento: !0
+
+Haciendo ping a 200.33.171.6 con 32 bytes de datos:
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+
+Estadísticas de ping para 200.33.171.6:
+    Paquetes: enviados = 4, recibidos = 4, perdidos = 0
+    (0% perdidos),
+200.33.171.6En funcionamiento: !0
+
+Haciendo ping a 200.33.171.7 con 32 bytes de datos:
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+
+Estadísticas de ping para 200.33.171.7:
+    Paquetes: enviados = 4, recibidos = 4, perdidos = 0
+    (0% perdidos),
+200.33.171.7En funcionamiento: !0
+
+Haciendo ping a 200.33.171.8 con 32 bytes de datos:
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+
+Estadísticas de ping para 200.33.171.8:
+    Paquetes: enviados = 4, recibidos = 4, perdidos = 0
+    (0% perdidos),
+200.33.171.8En funcionamiento: !0
+
+Haciendo ping a 200.33.171.9 con 32 bytes de datos:
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+
+Estadísticas de ping para 200.33.171.9:
+    Paquetes: enviados = 4, recibidos = 4, perdidos = 0
+    (0% perdidos),
+200.33.171.9En funcionamiento: !0
+
+Haciendo ping a 200.33.171.10 con 32 bytes de datos:
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Tiempo de espera agotado para esta solicitud.
+Tiempo de espera agotado para esta solicitud.
+
+Estadísticas de ping para 200.33.171.10:
+    Paquetes: enviados = 4, recibidos = 2, perdidos = 2
+    (50% perdidos),
+200.33.171.10En funcionamiento: !0
+
+Haciendo ping a 200.33.171.11 con 32 bytes de datos:
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+
+Estadísticas de ping para 200.33.171.11:
+    Paquetes: enviados = 4, recibidos = 4, perdidos = 0
+    (0% perdidos),
+200.33.171.11En funcionamiento: !0
+
+Haciendo ping a 200.33.171.12 con 32 bytes de datos:
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+
+Estadísticas de ping para 200.33.171.12:
+    Paquetes: enviados = 4, recibidos = 4, perdidos = 0
+    (0% perdidos),
+200.33.171.12En funcionamiento: !0
+
+Haciendo ping a 200.33.171.13 con 32 bytes de datos:
+Tiempo de espera agotado para esta solicitud.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+
+Estadísticas de ping para 200.33.171.13:
+    Paquetes: enviados = 4, recibidos = 3, perdidos = 1
+    (25% perdidos),
+200.33.171.13En funcionamiento: !0
+
+Haciendo ping a 200.33.171.14 con 32 bytes de datos:
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+
+Estadísticas de ping para 200.33.171.14:
+    Paquetes: enviados = 4, recibidos = 4, perdidos = 0
+    (0% perdidos),
+200.33.171.14En funcionamiento: !0
+
+Haciendo ping a 200.33.171.15 con 32 bytes de datos:
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+
+Estadísticas de ping para 200.33.171.15:
+    Paquetes: enviados = 4, recibidos = 4, perdidos = 0
+    (0% perdidos),
+200.33.171.15En funcionamiento: !0
+
+Haciendo ping a 200.33.171.16 con 32 bytes de datos:
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+
+Estadísticas de ping para 200.33.171.16:
+    Paquetes: enviados = 4, recibidos = 4, perdidos = 0
+    (0% perdidos),
+200.33.171.16En funcionamiento: !0
+
+Haciendo ping a 200.33.171.17 con 32 bytes de datos:
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+
+Estadísticas de ping para 200.33.171.17:
+    Paquetes: enviados = 4, recibidos = 4, perdidos = 0
+    (0% perdidos),
+200.33.171.17En funcionamiento: !0
+
+Haciendo ping a 200.33.171.18 con 32 bytes de datos:
+Tiempo de espera agotado para esta solicitud.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.97: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+
+Estadísticas de ping para 200.33.171.18:
+    Paquetes: enviados = 4, recibidos = 3, perdidos = 1
+    (25% perdidos),
+200.33.171.18En funcionamiento: !0
+
+Haciendo ping a 200.33.171.19 con 32 bytes de datos:
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Tiempo de espera agotado para esta solicitud.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+
+Estadísticas de ping para 200.33.171.19:
+    Paquetes: enviados = 4, recibidos = 3, perdidos = 1
+    (25% perdidos),
+200.33.171.19En funcionamiento: !0
+
+Haciendo ping a 200.33.171.20 con 32 bytes de datos:
+Respuesta desde 200.33.171.20: bytes=32 tiempo=1ms TTL=63
+Respuesta desde 200.33.171.20: bytes=32 tiempo<1m TTL=63
+Respuesta desde 200.33.171.20: bytes=32 tiempo<1m TTL=63
+Respuesta desde 200.33.171.20: bytes=32 tiempo<1m TTL=63
+
+Estadísticas de ping para 200.33.171.20:
+    Paquetes: enviados = 4, recibidos = 4, perdidos = 0
+    (0% perdidos),
+Tiempos aproximados de ida y vuelta en milisegundos:
+    Mínimo = 0ms, Máximo = 1ms, Media = 0ms
+200.33.171.20En funcionamiento: !0
+
+Haciendo ping a 200.33.171.21 con 32 bytes de datos:
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+
+Estadísticas de ping para 200.33.171.21:
+    Paquetes: enviados = 4, recibidos = 4, perdidos = 0
+    (0% perdidos),
+200.33.171.21En funcionamiento: !0
+
+Haciendo ping a 200.33.171.22 con 32 bytes de datos:
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+
+Estadísticas de ping para 200.33.171.22:
+    Paquetes: enviados = 4, recibidos = 4, perdidos = 0
+    (0% perdidos),
+200.33.171.22En funcionamiento: !0
+
+Haciendo ping a 200.33.171.23 con 32 bytes de datos:
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+
+Estadísticas de ping para 200.33.171.23:
+    Paquetes: enviados = 4, recibidos = 4, perdidos = 0
+    (0% perdidos),
+200.33.171.23En funcionamiento: !0
+
+Haciendo ping a 200.33.171.24 con 32 bytes de datos:
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+
+Estadísticas de ping para 200.33.171.24:
+    Paquetes: enviados = 4, recibidos = 4, perdidos = 0
+    (0% perdidos),
+200.33.171.24En funcionamiento: !0
+
+Haciendo ping a 200.33.171.25 con 32 bytes de datos:
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Tiempo de espera agotado para esta solicitud.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+
+Estadísticas de ping para 200.33.171.25:
+    Paquetes: enviados = 4, recibidos = 3, perdidos = 1
+    (25% perdidos),
+200.33.171.25En funcionamiento: !0
+
+Haciendo ping a 200.33.171.26 con 32 bytes de datos:
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+
+Estadísticas de ping para 200.33.171.26:
+    Paquetes: enviados = 4, recibidos = 4, perdidos = 0
+    (0% perdidos),
+200.33.171.26En funcionamiento: !0
+
+Haciendo ping a 200.33.171.27 con 32 bytes de datos:
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+
+Estadísticas de ping para 200.33.171.27:
+    Paquetes: enviados = 4, recibidos = 4, perdidos = 0
+    (0% perdidos),
+200.33.171.27En funcionamiento: !0
+
+Haciendo ping a 200.33.171.28 con 32 bytes de datos:
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+
+Estadísticas de ping para 200.33.171.28:
+    Paquetes: enviados = 4, recibidos = 4, perdidos = 0
+    (0% perdidos),
+200.33.171.28En funcionamiento: !0
+
+Haciendo ping a 200.33.171.29 con 32 bytes de datos:
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+
+Estadísticas de ping para 200.33.171.29:
+    Paquetes: enviados = 4, recibidos = 4, perdidos = 0
+    (0% perdidos),
+200.33.171.29En funcionamiento: !0
+
+Haciendo ping a 200.33.171.30 con 32 bytes de datos:
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+
+Estadísticas de ping para 200.33.171.30:
+    Paquetes: enviados = 4, recibidos = 4, perdidos = 0
+    (0% perdidos),
+200.33.171.30En funcionamiento: !0
+
+Haciendo ping a 200.33.171.31 con 32 bytes de datos:
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+
+Estadísticas de ping para 200.33.171.31:
+    Paquetes: enviados = 4, recibidos = 4, perdidos = 0
+    (0% perdidos),
+200.33.171.31En funcionamiento: !0
+
+Haciendo ping a 200.33.171.32 con 32 bytes de datos:
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+
+Estadísticas de ping para 200.33.171.32:
+    Paquetes: enviados = 4, recibidos = 4, perdidos = 0
+    (0% perdidos),
+200.33.171.32En funcionamiento: !0
+
+Haciendo ping a 200.33.171.33 con 32 bytes de datos:
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+
+Estadísticas de ping para 200.33.171.33:
+    Paquetes: enviados = 4, recibidos = 4, perdidos = 0
+    (0% perdidos),
+200.33.171.33En funcionamiento: !0
+
+Haciendo ping a 200.33.171.34 con 32 bytes de datos:
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+
+Estadísticas de ping para 200.33.171.34:
+    Paquetes: enviados = 4, recibidos = 4, perdidos = 0
+    (0% perdidos),
+200.33.171.34En funcionamiento: !0
+
+Haciendo ping a 200.33.171.35 con 32 bytes de datos:
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+
+Estadísticas de ping para 200.33.171.35:
+    Paquetes: enviados = 4, recibidos = 4, perdidos = 0
+    (0% perdidos),
+200.33.171.35En funcionamiento: !0
+
+Haciendo ping a 200.33.171.36 con 32 bytes de datos:
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+
+Estadísticas de ping para 200.33.171.36:
+    Paquetes: enviados = 4, recibidos = 4, perdidos = 0
+    (0% perdidos),
+200.33.171.36En funcionamiento: !0
+
+Haciendo ping a 200.33.171.37 con 32 bytes de datos:
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+
+Estadísticas de ping para 200.33.171.37:
+    Paquetes: enviados = 4, recibidos = 4, perdidos = 0
+    (0% perdidos),
+200.33.171.37En funcionamiento: !0
+
+Haciendo ping a 200.33.171.38 con 32 bytes de datos:
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+
+Estadísticas de ping para 200.33.171.38:
+    Paquetes: enviados = 4, recibidos = 4, perdidos = 0
+    (0% perdidos),
+200.33.171.38En funcionamiento: !0
+
+Haciendo ping a 200.33.171.39 con 32 bytes de datos:
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+
+Estadísticas de ping para 200.33.171.39:
+    Paquetes: enviados = 4, recibidos = 4, perdidos = 0
+    (0% perdidos),
+200.33.171.39En funcionamiento: !0
+
+Haciendo ping a 200.33.171.40 con 32 bytes de datos:
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+
+Estadísticas de ping para 200.33.171.40:
+    Paquetes: enviados = 4, recibidos = 4, perdidos = 0
+    (0% perdidos),
+200.33.171.40En funcionamiento: !0
+
+Haciendo ping a 200.33.171.41 con 32 bytes de datos:
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+
+Estadísticas de ping para 200.33.171.41:
+    Paquetes: enviados = 4, recibidos = 4, perdidos = 0
+    (0% perdidos),
+200.33.171.41En funcionamiento: !0
+
+Haciendo ping a 200.33.171.42 con 32 bytes de datos:
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+Respuesta desde 10.27.146.1: Host de destino inaccesible.
+
+Estadísticas de ping para 200.33.171.42:
+    Paquetes: enviados = 4, recibidos = 4, perdidos = 0
+    (0% perdidos),
   
